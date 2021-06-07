5eb22789db242268b5ad65dd5cde3df29e28af3c
jaeseokking <72733192+jaeseokking@users.noreply.github.com>
2021-06-07 17:31:05 +0900

    Commit before npm eject

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import SassComponent from './SassComponent'
+
+function App() {
+  return (
+    <div>
+      <SassComponent />
+    </div>
+  )
+}
+
+export default App
